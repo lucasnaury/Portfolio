08d35be1e22759f8f2cfc6a585e2a430e777afe3
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 <lucas.naury@sfr.fr>
2021-09-26 16:55:23 +0200

    Only play videos when 60% visible

--- js/main.js
+++ js/main.js
@@ -20,5 +20,36 @@ $(document).ready(function () {
     }
   });
 
+  $(window).scroll(playPauseVideo());
+
+    function playPauseVideo() {
+      let videos = document.querySelectorAll("video");
+      videos.forEach((video) => {
+          // We can only control playback without insteraction if video is mute
+          video.muted = true;
+          // Play is a promise so we need to check we have it
+          let playPromise = video.play();
+          if (playPromise !== undefined) {
+              playPromise.then((_) => {
+                  let observer = new IntersectionObserver(
+                      (entries) => {
+                          entries.forEach((entry) => {
+                              if (
+                                  entry.intersectionRatio !== 1 &&
+                                  !video.paused
+                              ) {
+                                  video.pause();
+                              } else if (video.paused) {
+                                  video.play();
+                              }
+                          });
+                      },
+                      { threshold: 0.6 }//Only play video if 60% visible
+                  );
+                  observer.observe(video);
+              });
+          }
+      });
+    }
 
 });
